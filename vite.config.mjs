4b504fdad5a5303897362fc4c5c4bea27f15a8e9
Richard <mugrich007@gmail.com>
2024-07-14 05:36:51 +0000

    nothing big

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0',
-        port: 3000,
-        open: true
-    }
-})
+  plugins: [react()],
+  build: {
+    outDir: 'build', // Specify the output directory as 'build' for production
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: true,
+  },
+});
