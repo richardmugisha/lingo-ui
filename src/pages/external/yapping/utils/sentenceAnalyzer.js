6304a30c5b6e1073b73bec2a25d71e6025ab774f
Richard <mugrich007@gmail.com>
2024-11-27 22:00:41 +0000

    fix(story-time): fix  edge case in removeKeyWord
    - when the last word of blankedSentence is dashed, checking the next
      word against the next word of fullSentence will throw indexError
    - so, when this scenario happens, it's safe to assume that all the next
      words are keywords

--- src/pages/external/yapping/utils/sentenceAnalyzer.js
+++ src/pages/external/yapping/utils/sentenceAnalyzer.js
@@ -9,19 +9,32 @@ export const getKeywords = (fullSentenceSplit, blankedSentenceSplit, currSentenc
     let sentIndex = 0
     let blankIndex = 0
     const missing = []
-    
+    // console.log('----------------------------started all over')
+    // console.log(fullSentenceSplit, blankedSentenceSplit, sentIndex, currSentence)
     while (blankIndex < blankedSentenceSplit.length && sentIndex < fullSentenceSplit.length) {
         // console.log(fullSentenceSplit, blankedSentenceSplit, sentIndex, currSentence)
         const currWord = fullSentenceSplit[sentIndex]
         const currBlank = blankedSentenceSplit[blankIndex]
         if (currWord !== currBlank) {
             let cont = currWord
-            while ( !(blankedSentenceSplit[blankIndex + 1] === fullSentenceSplit[sentIndex + 1] || ['_', '-', '['].includes(blankedSentenceSplit[blankIndex + 1][0]) ) && sentIndex < fullSentenceSplit.length) {
-                sentIndex++;
-                if (fullSentenceSplit[sentIndex]) cont += ' ' + fullSentenceSplit[sentIndex]
+            console.log(blankedSentenceSplit[blankIndex + 1], blankIndex+1)
+            if (blankIndex + 1 === blankedSentenceSplit.length) cont = fullSentenceSplit.slice(sentIndex, fullSentenceSplit.length).join(' ')
+            else {
+                while ( !(blankedSentenceSplit[blankIndex+1] === fullSentenceSplit[sentIndex + 1] || sentIndex < fullSentenceSplit.length && ['_', '-', '['].includes(blankedSentenceSplit[blankIndex + 1][0]) )) {
+                    // console.log('--------', cont, '/ ',  blankedSentenceSplit[blankIndex + 1], fullSentenceSplit[sentIndex + 1])
+                    // console.log('---------', fullSentenceSplit, blankedSentenceSplit, sentIndex)
+                    sentIndex++;
+                    if (fullSentenceSplit[sentIndex]) {
+                        cont += ' ' + fullSentenceSplit[sentIndex]
+                        // console.log('...............yeah', cont)
+                    }
+                }
             }
+            
+            // console.log('bibiib............', cont, sentIndex, blankIndex)
             missing.push(cont)
         }
+        // console.log('.....olol.....indices: ', sentIndex, blankIndex, fullSentenceSplit[sentIndex], blankedSentenceSplit[blankIndex])
         sentIndex++;
         blankIndex++;
     }
@@ -57,9 +70,12 @@ export const removeKeywords = (fullSentenceSplit, blankedSentenceSplit, currSent
         const currBlank = blankedSentenceSplit[blankIndex]
         if (currWord !== currBlank) {
             let cont = currWord
-            while ( !(blankedSentenceSplit[blankIndex + 1] === fullSentenceSplit[sentIndex + 1] || ['_', '-', '['].includes(blankedSentenceSplit[blankIndex + 1][0]) ) && sentIndex < fullSentenceSplit.length) {
-                sentIndex++;
-                if (fullSentenceSplit[sentIndex]) cont += ' ' + fullSentenceSplit[sentIndex]
+            if (blankIndex + 1 === blankedSentenceSplit.length) cont = fullSentenceSplit.slice(sentIndex, fullSentenceSplit.length).join(' ')
+            else {
+                while ( !(blankedSentenceSplit[blankIndex + 1] === fullSentenceSplit[sentIndex + 1] || ['_', '-', '['].includes(blankedSentenceSplit[blankIndex + 1][0]) ) && sentIndex < fullSentenceSplit.length) {
+                    sentIndex++;
+                    if (fullSentenceSplit[sentIndex]) cont += ' ' + fullSentenceSplit[sentIndex]
+                }
             }
             corrSent.push(cont)
             let dashed = cont.split('').map(char => ['.', ',', ';', ']', '"', '!', '?', ')'].includes(char) ? char : '_').join('')
